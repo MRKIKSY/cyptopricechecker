12ff93b59221636493e09eebeeb0fa9773164937
SAMUEL AKINOLA ELUYEFA <149276078+MRKIKSY@users.noreply.github.com>
2024-04-27 11:20:42 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from 'axios';
-import '../src/app.css';
+import '../app.css';
 
 function App() {
   const [search, setSearch] = useState("");
