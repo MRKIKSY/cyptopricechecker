d0fba2b91d1638af320b25f2c7b86021baca1b1f
SAMUEL AKINOLA ELUYEFA <149276078+MRKIKSY@users.noreply.github.com>
2024-04-27 14:32:31 +0100

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from 'axios';
-import '../src/app.css';
+import "./App.css";
 
 function App() {
   const [search, setSearch] = useState("");
